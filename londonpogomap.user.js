674986f9d5a797748c78a276b1df179745c771c5
Slawomir Malusecki <s.malusecki@quidco.com>
2017-12-28 09:36:43 +0000

    feat(LondonPogoMap): Hide additional elements

--- londonpogomap.user.js
+++ londonpogomap.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LondonPogoMap Enchancer
 // @namespace    https://github.com/moriakaice/user-scripts
-// @version      1.0.0
+// @version      1.1.0
 // @description  Icons for LondonPogoMap + expand the map area
 // @author       moriakaice
 // @match        https://londonpogomap.com/*
@@ -9,7 +9,11 @@
 // ==/UserScript==
 
 (function ($) {
-  $('#overlay').remove(); $('#please_top').remove(); $('#please_bottom').remove(); $('#map').attr('style', '');$('#toast-container').attr('style', 'display: none;');
+  $('#overlay').remove(); $('.please').remove(); $('#map').attr('style', '');
+
+  var styles = document.createElement('style')
+  styles.innerHTML = '#overlay, #toast-container, .please {display: none !important;}'
+  document.querySelector('head').appendChild(styles)
 
   for (var i = 1; i < 1000; i++) { localStorage.setItem('icon' + i, '//pkmref.com/images/set_1/'+i+'.png'); }
 })($);
